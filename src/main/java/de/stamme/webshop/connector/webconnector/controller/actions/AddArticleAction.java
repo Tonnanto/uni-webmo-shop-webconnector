abb598609e6df532514c35fb09f4bc1f15ea68b1
Anton Stamme <anton@stamme.de>
2021-06-14 14:13:00 +0200

    Refactored "Add Article" & "Remove Article" UseCases

--- src/main/java/de/stamme/webshop/connector/webconnector/controller/actions/AddArticleAction.java
+++ src/main/java/de/stamme/webshop/connector/webconnector/controller/actions/AddArticleAction.java
@@ -1,8 +1,25 @@
 package de.stamme.webshop.connector.webconnector.controller.actions;
 
+
+import de.leuphana.shop.structure.Article;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("addArticleAction")
 public class AddArticleAction extends WebshopAction {
     @Override
     protected String doAction() {
-        return null;
+        try {
+            int articleId = Integer.parseInt(request.getParameter("articleId"));
+            Integer customerId = getCustomerIdFromSession();
+            Article article = onlineShop.getArticle(articleId);
+
+            onlineShop.addArticleToCart(customerId, articleId);
+
+            request.setAttribute("addedArticle", article);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid article ID: " + request.getParameter("articleId"));
+        }
+        return "/showCatalog";
     }
 }
--- src/main/java/de/stamme/webshop/connector/webconnector/controller/actions/RemoveArticleAction.java
+++ src/main/java/de/stamme/webshop/connector/webconnector/controller/actions/RemoveArticleAction.java
@@ -1,8 +1,22 @@
 package de.stamme.webshop.connector.webconnector.controller.actions;
 
+
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet("removeArticleAction")
 public class RemoveArticleAction extends WebshopAction {
     @Override
     protected String doAction() {
-        return null;
+
+        try {
+            int articleId = Integer.parseInt(request.getParameter("articleId"));
+            int customerId = getCustomerIdFromSession();
+            onlineShop.decrementArticleQuantityInCart(customerId, articleId);
+
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid article ID: " + request.getParameter("articleId"));
+        }
+
+        return "/showCart";
     }
 }
